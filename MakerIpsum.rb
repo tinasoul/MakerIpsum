f69e3f103ec69c385d6720a93cae3af721e81c47
MakerSquare Student <student@macintosh.att.net>
2013-06-25 16:19:33 -0500

    t quit exit "we struggled"

--- MakerIpsum.rb
+++ MakerIpsum.rb
@@ -19,7 +19,10 @@ end
 #  		csv << ["handle", "text", "url"]
 
 
-ipsum = ["that's so bootstrap", "HAHA business", "alright alright", "I don't speak frat", "dogs drink superwater", "devhouse party", "D-Money", "ruby redbird", "git push origin master", "git push and coast"]
+# ipsum = ["that's so bootstrap", "HAHA business", "alright alright", "I don't speak frat", "dogs drink superwater", "devhouse party", "D-Money", "ruby redbird", "git push origin master", "git push and coast"]
+
+# ipsum = []
+# array = ["#"]
 
 # puts "How many paragraphs do you want?"
 
@@ -30,10 +33,41 @@ ipsum = ["that's so bootstrap", "HAHA business", "alright alright", "I don't spe
 # end} }
 		
 Twitter.search("#makeripsum").results.map do |tweet|
-	ipsum << [tweet.full_text]
-	if tweet.full_text == *'@' && *'#' && *'&lt'
-	puts ipsum
-	end
+	a = tweet.full_text.inspect
+	b = a.split(" ")
+	b.delete_if {|word| word == "#makeripsum"}
+	print b
+end
+
+
+
+
+
+	# print a.split(" ")
+	# a.delete_if{|string| string.include?"@"}
+	# puts a.inspect
+
+# 	array_of_words = a.split(' ')
+# 	# array of words could be ['hey', 'whats', 'up', '@makersquare']
+# 	array_of_words.delete_if do |word|
+# 		# Last line should be true if you want to remove from array
+# 	end
+# 	# array of words should be ['hey', 'whats', 'up']
+# 	a.split.delete_if{|string| array.include?("#")}.join(' ')
+# end
+
+# array.delete_if do |x|
+# 	x.blah
+# end
+
+# array.each {|x| x.blah}
+
+
+	# ipsum << tweet.full_text
+	# a = tweet.full_text.split(" ")
+	# puts a.inspect
+	# end
+
 
 # def hashtags
 #   @hashtags ||= entities(Twitter::Entity::Hashtag, :hashtags)
