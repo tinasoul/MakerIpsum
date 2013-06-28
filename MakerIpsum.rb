2541cb5651b56532e1ecfad04848d260e7738f0e
sagarispatel <sagarjppatel@gmail.com>
2013-06-28 13:47:22 -0500

    added a pick your hashtag function for ipsum searches

--- MakerIpsum.rb
+++ MakerIpsum.rb
@@ -1,109 +1,110 @@
+require 'twitter'
+
+
+Twitter.configure do |config|
+	config.consumer_key = ENV['CONFIG_CONSUMER_KEY']
+	config.consumer_secret = ENV['CONFIG_CONSUMER_SECRET']
+	config.oauth_token = ENV['CONFIG_OAUTH_TOKEN']
+	config.oauth_token_secret = ENV['CONFIG_OAUTH_TOKEN_SECRET']
+end
+
+
 # ipsum = ["that's so bootstrap", "HAHA business", "alright alright", "I don't speak frat", "dogs drink superwater", "D-Money", "ruby redbird", "chi'lantro", "git push and coast"]
 
-# india = ["limbu paani", "pav baji", "Amitabh Bachchan", "bollywood", "shah", "patel", "parekh", "dot on your forehead", "elephant", "monkey", "rickshaw", "brown people spicy?", "bhangra","curry", "mangoooooo", "Snapshot India", "slumdog app developer", "thank you come again", "hotel/motel", "aunty"]
+ipsum = []
 
-class Ipsum
-  # class info
-  @@dummy_ipsum = ["that's so bootstrap.", "HAHA business.", "alright alright.", "I don't speak frat.", "dogs drink superwater.", "D-Money.", "ruby redbird.", "chi'lantro.", "git push and coast."]
-  @@final_sentence
+india = ["limbu paani", "pav baji", "Amitabh Bachchan", "bollywood", "shah", "patel", "parekh", "dot on your forehead", "elephant", "monkey", "rickshaw", "brown people spicy?", "bhangra","curry", "mangoooooo", "Snapshot India", "slumdog app developer", "thank you come again", "hotel/motel", "aunty"]
 
 
-  def self.make_a_paragraph(number_of_paras)
-    number_of_paras.times do
-      5.times do 
-        make_a_sentence
-      end
-      puts " "
-      puts " "
-    end
-  end
-
-  def self.make_a_sentence
-    raw_sentence = @@dummy_ipsum.sample(5).join(" ")
-    capitalized_sentence = raw_sentence.capitalize!
+puts "What ipsum do you want. Enter a twitter hashtag"
+@response= gets.chomp
 
 
-    punctuated_sentence = "#{capitalized_sentence}#{%w{. ? !}[rand(0..2)]}"
-    @@final_sentence = punctuated_sentence + " "
+Twitter.search(@response, :count => 10000).results.each do |tweet| #how do we include all tweets without setting a max count
+  orig_string = tweet.full_text
+  split_string = orig_string.split(" ")
+  split_string.reject! do |word|
+    # word == "#makeripsum" or word == "@makersquare" or --looks for exactness which is what we don't want!
+    word.include?("#") or word.include?("RT") or word.include?("http") or word.include?("&") or word.include?("@")
+  end 
+  joined_string = split_string.join(" ")
+  ipsum << joined_string
+end
 
-    
-    final_sentence_array = []
-    final_sentence_array << @@final_sentence
-    print final_sentence_array
-  end
+
+puts "How many paragraphs do you want?"
+
+answer = gets.chomp.to_i
+
+puts "With a shot of India? (yes or no)"
+
+answer2 = gets.downcase.chomp
+
+if answer2.chars.first == "y"
+	ipsum.concat(india)
+
+	answer.times do
+		puts "\n"
+		puts "\n"
+		ipsum.sample(21).each do |thing|
+			print "#{thing} "
+			# can we create a method that does this so we don't repeat code.	
+		end
+	end	
+
+else
+	answer.times do
+		puts "\n"
+		puts "\n"
+		ipsum.sample(21).each do |thing|
+			print "#{thing} "
+		end
+	end
 end
 
 
 
-#   # has paragraphs
-#   def paragraphs(n)
-#     # generate n paragraphs
-#     paragraphs = Array.new(n) { |index|
-#       Paragraph.new.sentences( rand(15..20) )
-#     }
-#   end
+#Next steps:
+# Capitalize first work in para and end each para with a "."
 
+
+
+# Shehzan breaking is down for us
+
+# 	array_of_words = a.split(' ')
+# 	# array of words could be ['hey', 'whats', 'up', '@makersquare']
+# 	array_of_words.delete_if do |word|
+# 		# Last line should be true if you want to remove from array
+# 	end
+# 	# array of words should be ['hey', 'whats', 'up']
+# 	a.split.delete_if{|string| array.include?("#")}.join(' ')
 # end
 
-# class Paragraph
-#   # class info
-
-#   # instance info
-#   # has sentences
-#   def sentences(n)
-#     # generate n sentences
-#     sentences = Array.new(n) { |index|
-#       Sentence.new.words( rand(10..15) )
-#     }
-#     sentences.join(' ')
-#   end
-
+# array.delete_if do |x|
+# 	x.blah
 # end
 
-# class Sentence
-#   # class info
-
-#   # instance info
-#   # has words
-
-#   def words(n)
-#     # generate n sentences
-#     words = Array.new(n) { |index|
-#       Word.new
-#     }
-#     words[0].capitalize!               # capitalize first word
-#     words[-1] += %w{. ? !}[rand(0..2)] # add ending punctuation
-#     2.times {
-#       # add middle punctuation
-#       words[rand(0..words.count-2)] += %w{, ; : \ - ...}[rand(0..4)]
-#     }
-#     words
-#   end
-# end
-
-# class Word
-#   # class info
-
-#   def self.new
-#     Ipsum.dictionary[ rand( 0..Ipsum.dictionary.count-1 ) ]
-#   end
-
-#   # instance info
-# end
+# array.each {|x| x.blah}
 
 
-# # --------- Main ---------- #
 
-# # Ipsum.new.paragraphs(n) => array of n paragraphs
-# paragraphs = Ipsum.new.paragraphs(2)
+# Original ruby code before we linked to twitter api 
 
-# # render to console
-# paragraphs.each { |p| puts p; puts }
+# ipsum = []
 
-# # to render in rails, iterate over array of paragraphs
-# # <% Ipsum.new.paragraphs(2).each do |p| %>
-# #   <p><%= p %></p>
-# # <% end %>
+# puts "How many paragraphs do you want?"
 
-# # --------- Test ---------- #
-# # Ipsum.new
+# answer = gets.chomp.to_i
+
+# answer.times {5.times {ipsum.sample(5).each do |thing|
+# 	print "#{thing} "
+# end} 
+
+
+# How many paragraphs do you want?
+# # 	gets answer
+# i.e. # if 3 then run above loop 3 times
+
+# sentence = ipsum.sample(5).join(" ")
+# puts sentence
+# 5.times {print sentence} ----> prints the SAME sentence in a row 5 times
